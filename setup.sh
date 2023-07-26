9ee817d670e9e626530dc3e3a97abef3724f03e3
Ludovico Mori <mori25@mit.edu>
2023-07-26 17:06:03 -0400

    Correct env var

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #export A2RCHI_BASE=$HOME/Work/A2rchi
-export A2RCHI_BASE=/work/submit/juliush/A2rchi
-export OPEN_AI_KEY=`cat $HOME/.openai/api.key`
+export A2RCHI_BASE=/work/submit/mori25/A2rchi
+export OPENAI_API_KEY=`cat $HOME/.openai/api.key`
 export PATH="${PATH}:${A2RCHI_BASE}/bin"
 export PYTHONPATH="${PYTHONPATH}:${A2RCHI_BASE}/interfaces:${PYTHONPATH}:${A2RCHI_BASE}/utils:${A2RCHI_BASE}/utils"
 
-source $HOME/.cleo
-source $HOME/.imap
-source $HOME/.sender
+#source $HOME/.cleo
+#source $HOME/.imap
+#source $HOME/.sender
