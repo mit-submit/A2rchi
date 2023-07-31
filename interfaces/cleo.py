7963e7f35d9b0e2586b686038e38765517458156
Julius Heitkoetter <juliush@mit.edu>
2023-07-31 12:09:14 -0400

    adding checking codes against names

--- interfaces/cleo.py
+++ interfaces/cleo.py
@@ -9,11 +9,11 @@ import numpy as np ##TODO: remove this
 from chains.chain import Chain
 
 a2rchi_pattern = '-- A2rchi --'
-new_status_id = 1        # (or 1 this is after first work)
-inprogress_status_id = 2 # in progress
-feedback_status_id = 4   # feedback
-resolved_status_id = 3   # resolved
-closed_status_id = 5     # closed
+#new_status_id = 1        # (or 1 this is after first work)
+#inprogress_status_id = 2 # in progress
+#feedback_status_id = 4   # feedback
+#resolved_status_id = 3   # resolved
+#closed_status_id = 5     # closed
 normal_priority_id = 2   # normal priority
 support_tracker_id = 3   # tracker id for 'Support'
 
@@ -76,19 +76,37 @@ class Cleo:
             self.redmine = self._connect()
             self.user = self.redmine.user.get('current')
             self.load()
+
+        #Load all the status, tracker, and priority ids
+        statuses = self.redmine.issue_status.all()
+        self.status_dict = dict()     # keys = status name, values = status_id
+        for s in statuses:
+            self.status_dict[s.name] = s.id
+
+        trackers = self.redmine.tracker.all()
+        self.tracker_dict = dict()
+        for t in trackers:
+            self.tracker_dict[t.name] = t.id
+
+        priorities = self.redmine.enumeration.filter(resource="issue_priorities")
+        self.priorities_dict = dict()
+        for p in priorities:
+            self.priorities_dict[p.name] = p.id
+
+
             
     def add_note_to_issue(self,issue_id,note):
         """
         Adding a note to an existing issue (and move to 'feedback' status)
         """
-        self.redmine.issue.update(issue_id,status_id=feedback_status_id,notes=note)
+        self.redmine.issue.update(issue_id,status_id=self.status_dict['Feedback'],notes=note)
         return
 
     def reopen_issue(self, issue_id, note):
         """
         Move an issues status to `In Progress` and add a note
         """
-        self.redmine.issue.update(issue_id,status_id=inprogress_status_id,notes=note)
+        self.redmine.issue.update(issue_id,status_id=self.status_dict['In Progress'],notes=note)
         return
     
     def get_issue_history(self,issue_id):
@@ -119,9 +137,9 @@ class Cleo:
         issue.project_id = self.project.id
         issue.subject = subject
         issue.description = description
-        issue.tracker_id = support_tracker_id
-        issue.status_id = new_status_id
-        issue.priority_id = normal_priority_id
+        issue.tracker_id = self.tracker_dict["Support"]
+        issue.status_id = self.status_dict['New']
+        issue.priority_id = self.priorities_dict['Normal']
         issue.assigned_to_id = self.user.id
         issue.watcher_user_ids = []
         #issue.parent_issue_id =
@@ -143,7 +161,7 @@ class Cleo:
         """
         issue_ids = []
         for issue in self.redmine.issue.filter(assigned_to_id=self.user.id,):
-            if issue.status.id == new_status_id or issue.status.id == inprogress_status_id:
+            if issue.status.id == self.status_dict['New'] or issue.status.id == self.status_dict['In Progress']:
                 issue_ids.append(issue.id)
                 subject = f"Re:{issue.subject}"
                 history = self.remove_format(f"description: {issue.description}",'pre')
@@ -164,7 +182,7 @@ class Cleo:
         """
         issue_ids = []
         for issue in self.project.issues:
-            if issue.status.id == resolved_status_id:
+            if issue.status.id == self.status_dict['Resolved']:
                 print(f" process_resolved_issues: {issue.id}")
                 issue_ids.append(issue.id)
                 subject = f"Re:{issue.subject}"
@@ -190,7 +208,7 @@ class Cleo:
         """
         Moving the issue in the 'closed' status
         """
-        self.redmine.issue.update(issue_id,status_id=closed_status_id,
+        self.redmine.issue.update(issue_id,status_id=self.status_dict['Closed'],
                                   notes=f'{a2rchi_pattern} Resolving email was sent.')
         return
     
@@ -198,7 +216,7 @@ class Cleo:
         """
         Moving the issue in the 'feedback' status
         """
-        self.redmine.issue.update(issue_id,status_id=feedback_status_id,
+        self.redmine.issue.update(issue_id,status_id=self.status_dict['Feedback'],
                                   notes=f'{a2rchi_pattern} Moved into feedback.')
         return
 
