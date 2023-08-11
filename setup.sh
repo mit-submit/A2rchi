cfe69963cb0c293743ea87c1c482b4985fbcc45f
paus <paus@mit.edu>
2023-08-11 07:38:11 -0400

    Generate setup at install.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#export A2RCHI_BASE=$HOME/Work/A2rchi
-export A2RCHI_BASE=/work/submit/mori25/A2rchi
-export OPENAI_API_KEY=`cat $HOME/.openai/api.key`
-export PATH="${PATH}:${A2RCHI_BASE}/bin"
-export PYTHONPATH="${PYTHONPATH}:${A2RCHI_BASE}/interfaces:${PYTHONPATH}:${A2RCHI_BASE}/utils:${A2RCHI_BASE}/utils"
-
-source $HOME/.cleo
-source $HOME/.imap
-source $HOME/.sender
