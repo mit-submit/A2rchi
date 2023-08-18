10b18caaddbc520f5635c56cb77d888c497abae4
mdr223 <mdr223@cornell.edu>
2023-08-18 09:30:03 -0400

    removing copy to see if ssh script fails

---
+++ deploy/dev-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo `pwd`
+ls
+# tmux new-session -d -s chat-service './deploy/run_chat.sh'
+# tmux new-session -d -s cleo-service './deploy/run_cleo.sh'
+# tmux new-session -d -s mailer-service './deploy/run_mailbox.sh'
+# tmux new-session -d -s scraper-service './deploy/run_scraper.sh'
---
+++ deploy/run_chat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source venv/bin/activate
+python bin/service_chat.py
