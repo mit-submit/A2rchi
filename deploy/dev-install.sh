573dbe30218972555fdd6bd8203d1bdf6a69871a
mdr223 <mdr223@cornell.edu>
2023-08-19 16:34:57 -0400

    adding --build flag to have docker rebuild images

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -4,7 +4,7 @@ echo "Going into A2rchi directory"
 cd A2rchi
 echo "Starting docker compose"
 cd deploy
-docker compose up -d
+docker compose up -d --build
 
 # # secrets files are created by CI pipeline and destroyed here
 # rm cleo_url.txt
