8c4a4c6d01bef60f8afa70c80b8da618f527a663
mdr223 <mdr223@cornell.edu>
2023-08-25 12:16:58 -0400

    trying another approach to force recreate containers

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -4,9 +4,9 @@ echo "Going into A2rchi directory"
 cd A2rchi
 echo "Starting docker compose"
 cd deploy
-docker-compose rm -f
-docker-compose pull
-docker compose up -d --build --no-cache
+docker compose rm -f
+docker compose pull
+docker compose up -d --build
 
 # secrets files are created by CI pipeline and destroyed here
 rm cleo_url.txt
