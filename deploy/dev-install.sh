a5c85eae8288b95ce3f04df10738506538db3191
mdr223 <mdr223@cornell.edu>
2023-08-25 11:56:35 -0400

    add openai api key

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -6,8 +6,9 @@ echo "Starting docker compose"
 cd deploy
 docker compose up -d --build
 
-# # secrets files are created by CI pipeline and destroyed here
-# rm cleo_url.txt
-# rm cleo_user.txt
-# rm cleo_pw.txt
-# rm cleo_project.txt
+# secrets files are created by CI pipeline and destroyed here
+rm cleo_url.txt
+rm cleo_user.txt
+rm cleo_pw.txt
+rm cleo_project.txt
+rm openai_api_key.txt
