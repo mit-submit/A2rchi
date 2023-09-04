f77ba61ed2baed50105fdf572d457b546d343f9c
mdr223 <mdr223@cornell.edu>
2023-09-04 11:57:02 -0400

    updating dockerfile

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -4,8 +4,6 @@ echo "Going into A2rchi directory"
 cd A2rchi
 echo "Starting docker compose"
 cd deploy
-docker compose rm -f
-docker compose pull
 docker compose up -d --build
 
 # secrets files are created by CI pipeline and destroyed here
