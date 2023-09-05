8f1dffcac86f02695ba119104c6df4963854ae84
mdr223 <mdr223@cornell.edu>
2023-09-04 21:25:03 -0400

    uncomment rm secrets for debugging

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -4,9 +4,9 @@ echo "Starting docker compose"
 cd A2rchi/deploy/
 docker compose up -d --build
 
-# secrets files are created by CI pipeline and destroyed here
-rm cleo_url.txt
-rm cleo_user.txt
-rm cleo_pw.txt
-rm cleo_project.txt
-rm openai_api_key.txt
+# # secrets files are created by CI pipeline and destroyed here
+# rm cleo_url.txt
+# rm cleo_user.txt
+# rm cleo_pw.txt
+# rm cleo_project.txt
+# rm openai_api_key.txt
