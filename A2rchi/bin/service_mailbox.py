7974d23497685654ad293de0d35dd57ac4ffce3b
mdr223 <mdr223@cornell.edu>
2023-09-06 12:42:22 -0400

    add os.environ setting of openai key

--- A2rchi/bin/service_mailbox.py
+++ A2rchi/bin/service_mailbox.py
@@ -1,10 +1,14 @@
 #!/bin/python
 from A2rchi.interfaces import cleo
 from A2rchi.utils import mailbox
+from A2rchi.utils.env import read_secret
 
 import os
 
+# set openai
+os.environ['OPENAI_API_KEY'] = read_secret("OPENAI_API_KEY")
 print("Starting Mailbox Service")
+
 cleo = cleo.Cleo('Cleo_Helpdesk')
 
 while True:
