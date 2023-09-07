f3d641b77284ae5a3484a49300827ebfae4712e9
mdr223 <mdr223@cornell.edu>
2023-09-07 11:09:36 -0400

    add volume to share data between scraper, chat, cleo

--- deploy/dev-install.sh
+++ deploy/dev-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# create volume if it doesn't already exist
+exists=`docker volume ls | awk '{print $2}' | grep a2rchi-data`
+if [[ $exists != 'a2rchi-data' ]]; then
+    docker volume create --name a2rchi-data
+fi
+
+# start services
 echo "Starting docker compose"
 cd A2rchi/deploy/
 docker compose up -d --build --force-recreate --always-recreate-deps
