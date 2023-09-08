b8b3f078cfaf1f7e765380e90d428abb3d6cac1b
mdr223 <mdr223@cornell.edu>
2023-09-08 15:16:06 -0400

    remove sleep for mailbox

--- A2rchi/bin/service_mailbox.py
+++ A2rchi/bin/service_mailbox.py
@@ -12,8 +12,6 @@ print("Starting Mailbox Service")
 cleo = cleo.Cleo('Cleo_Helpdesk')
 
 while True:
-    import time
-    time.sleep(3600)
     mail = mailbox.Mailbox()
     mail.process_messages(cleo)
     os.system("sleep 10")
