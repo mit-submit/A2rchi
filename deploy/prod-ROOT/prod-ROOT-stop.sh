a86373f6a249e72392f8c0273fbfe38f2ae85c47
Ludovico Mori <mori25@mit.edu>
2023-10-02 17:47:01 -0400

    commented delete action just for first deployment

--- deploy/prod-ROOT/prod-ROOT-stop.sh
+++ deploy/prod-ROOT/prod-ROOT-stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Stop running docker compose"
-cd A2rchi-prod-ROOT/deploy/prod-ROOT/
-docker compose -f prod-ROOT-compose.yaml down
+# cd A2rchi-prod-ROOT/deploy/prod-ROOT/
+# docker compose -f prod-ROOT-compose.yaml down
