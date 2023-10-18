fe2251f13778a45a7b796b84000ff17678dd4a3a
Julius Heitkoetter <juliush@mit.edu>
2023-10-18 14:30:45 -0400

    fixing typo

--- A2rchi/interfaces/chat_app/app.py
+++ A2rchi/interfaces/chat_app/app.py
@@ -73,9 +73,9 @@ class ChatWrapper:
         # update or add discussion
         discussion_dict = data.get(str(discussion_id), {})
 
-        discussion_id["meta"] = discussion_dict.get("meta", {})
+        discussion_dict["meta"] = discussion_dict.get("meta", {})
         if str(discussion_id) not in discussion_dict.keys(): #first time in discusssion
-            discussion_id["meta"]["time_first_used"] = time.time()
+            discussion_dict["meta"]["time_first_used"] = time.time()
         discussion_dict["meta"]["time_last_used"] = time.time()
 
         if discussion_contents is not None:
