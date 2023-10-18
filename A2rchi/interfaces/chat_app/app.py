9acbf7a34d12ff2e938bd41ab793cae5c0eb0047
Julius Heitkoetter <juliush@mit.edu>
2023-10-18 10:30:27 -0400

    adding timestamp

--- A2rchi/interfaces/chat_app/app.py
+++ A2rchi/interfaces/chat_app/app.py
@@ -11,6 +11,7 @@ import numpy as np
 import json
 import os
 import yaml
+import time
 
 # DEFINITIONS
 QUERY_LIMIT = 1000 # max number of queries 
@@ -71,6 +72,8 @@ class ChatWrapper:
 
         # update or add discussion
         discussion_dict = data.get(str(discussion_id), {})
+
+        discussion_dict["meta"] = {"time_last_used", time.time()}
 
         if discussion_contents is not None:
             print(" INFO - found contents.")
