679991a501d92ea909ea84568580004f900c1527
mdr223 <mdr223@cornell.edu>
2023-10-22 19:36:16 -0400

    adding initial work for installing postgres db in dev; not pushing to it yet

---
+++ deploy/dev/dev-init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS conversations (
+    conversation_id INTEGER NOT NULL,
+    message_id INTEGER NOT NULL,
+    sender TEXT NOT NULL, -- eventually put foreign key constraint on users table
+    content TEXT NOT NULL,
+    ts TIMESTAMP NOT NULL,
+    PRIMARY KEY (conversation_id, message_id)
+);
+CREATE TABLE IF NOT EXISTS feedback (
+    cid INTEGER NOT NULL,
+    mid INTEGER NOT NULL,
+    feedback_ts TIMESTAMP NOT NULL,
+    feedback TEXT NOT NULL,
+    message TEXT,
+    incorrect BOOLEAN,
+    unhelpful BOOLEAN,
+    inappropriate BOOLEAN,
+    PRIMARY KEY (cid, mid, feedback_ts)
+    FOREIGN KEY (cid, mid) REFERENCES conversations(conversation_id, message_id) 
+);
