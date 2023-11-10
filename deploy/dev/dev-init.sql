cc9b6666b1fd284560a9762f241a83018f256ac0
mdr223 <mdr223@cornell.edu>
2023-11-10 11:44:36 -0500

    initial attempt to add grafana with postgres data source

--- deploy/dev/dev-init.sql
+++ deploy/dev/dev-init.sql
@@ -1,3 +1,16 @@
+-- create grafana user if it does not exist
+DO
+$do$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'grafana') THEN
+        CREATE USER grafana WITH PASSWORD 'GRAFANA_PASSWORD';
+        GRANT USAGE ON SCHEMA public TO grafana;
+        GRANT SELECT ON public.timing TO grafana;
+    END IF;
+END
+$do$;
+
+-- create tables
 CREATE TABLE IF NOT EXISTS conversations (
     conversation_id INTEGER NOT NULL,
     message_id SERIAL,
--- deploy/dev/dev-install.sh
+++ deploy/dev/dev-install.sh
@@ -12,6 +12,19 @@ if [[ $exists != 'a2rchi-dev-pg-data' ]]; then
     docker volume create --name a2rchi-dev-pg-data
 fi
 
+# create volume if it doesn't already exist for grafana data
+exists=`docker volume ls | awk '{print $2}' | grep a2rchi-dev-grafana-data`
+if [[ $exists != 'a2rchi-dev-grafana-data' ]]; then
+    docker volume create --name a2rchi-dev-grafana-data
+fi
+
+# fill-in variables in grafana files
+export grafanapass=`cat A2rchi-dev/deploy/dev/secrets/grafana_password.txt`
+sed -i 's/GRAFANA_PASSWORD/'"${grafanapass}"'/g' A2rchi-dev/deploy/grafana/datasources.yaml
+sed -i 's/GRAFANA_PASSWORD/'"${grafanapass}"'/g' A2rchi-dev/deploy/dev/dev-init.sql
+sed -i 's/RUNTIME_ENV/dev/g' A2rchi-dev/deploy/grafana/datasources.yaml
+unset grafanapass
+
 # build base image; try to reuse previously built image
 cd A2rchi-dev/deploy/dev/
 docker build -f ../dockerfiles/Dockerfile-base -t a2rchi-base:BASE_TAG ../..
