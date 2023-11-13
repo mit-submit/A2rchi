a678a9115b0d3c9faee5634faae90eda291cdfa2
mdr223 <mdr223@cornell.edu>
2023-11-13 10:59:22 -0500

    trying to fix substitution

--- deploy/create_secret.sh
+++ deploy/create_secret.sh
@@ -4,6 +4,6 @@ secret_name=$1
 secret_file=$2
 env=$3
 
-touch ${{ github.workspace }}/deploy/${env}/secrets/${secret_file}
-echo "${{ secrets.${secret_name} }}" >> ${{ github.workspace }}/deploy/${env}/secrets/${secret_file}
-chmod 400 ${{ github.workspace }}/deploy/${env}/secrets/${secret_file}
+touch ${{ github.workspace }}/deploy/"${env}"/secrets/"${secret_file}"
+echo "${{ secrets.${secret_name} }}" >> ${{ github.workspace }}/deploy/"${env}"/secrets/"${secret_file}"
+chmod 400 ${{ github.workspace }}/deploy/"${env}"/secrets/"${secret_file}"
