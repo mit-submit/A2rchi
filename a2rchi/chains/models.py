d57705aad6b993ed838ed959da9a280f72d39917
Pietro Martino Lugato <pmlugato@submit77.mit.edu>
2025-03-24 11:12:56 -0400

    generic hugging face model class

--- a2rchi/chains/models.py
+++ a2rchi/chains/models.py
@@ -74,7 +74,7 @@ class LlamaLLM(BaseCustomLLM):
     enable_salesforce_content_safety: bool = True
                                     # enable safety check with Salesforce safety flan t5
     quantization: bool = True       # enables 8-bit quantization
-    max_new_tokens: int = 4096      # maximum numbers of tokens to generate
+    max_new_tokens: int = 2048      # maximum numbers of tokens to generate
     seed: int = None                # seed value for reproducibility
     do_sample: bool = True          # use sampling; otherwise greedy decoding
     min_length: int = None          # minimum length of sequence to generate, input prompt + min_new_tokens
@@ -107,7 +107,8 @@ class LlamaLLM(BaseCustomLLM):
         # create tokenizer
         self.tokenizer = None
         self.tokenizer = LlamaTokenizer.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False)
-        base_model = LlamaForCausalLM.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False, load_in_8bit=self.quantization, device_map='auto', torch_dtype = torch.float16)
+        # removed "load_in_8bit" argument, need to install correct bitsandbytes version then use BitsAndBytesConfig object and pass that
+        base_model = LlamaForCausalLM.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False, device_map='auto', torch_dtype = torch.float16)
         if self.peft_model:
             self.llama_model = PeftModel.from_pretrained(base_model, self.peft_model)
         else:
@@ -185,6 +186,165 @@ class LlamaLLM(BaseCustomLLM):
                         - reformat your question so that it does not prompt an unsafe response."""
 
         return output_text[output_text.rfind("[/INST]") + len("[/INST]"):]
+
+
+class HuggingFaceOpenLLM(BaseCustomLLM):
+    """
+    Loading any chat-based LLM available on Hugging Face. Make sure that the model
+    is downloaded and the base_model_path is linked to correct model.
+    Pick your favorite: https://huggingface.co/spaces/open-llm-leaderboard/open_llm_leaderboard#/
+    Note you might need to change other parameters like max_new_tokens, prompt lengths, or other model specific parameters.
+    """
+    base_model: str = None     # location of the model (ex. meta-llama/Llama-2-70b)
+    peft_model: str = None          # location of the finetuning of the model 
+    enable_salesforce_content_safety: bool = True
+                                    # enable safety check with Salesforce safety flan t5
+    quantization: bool = True       # enables 8-bit quantization
+    max_new_tokens: int = 2048      # maximum numbers of tokens to generate
+    seed: int = None                # seed value for reproducibility
+    do_sample: bool = True          # use sampling; otherwise greedy decoding
+    min_length: int = None          # minimum length of sequence to generate, input prompt + min_new_tokens
+    use_cache: bool = True          # [optional] model uses past last key/values attentions
+    top_p: float = .9               # [optional] for float < 1, only smallest set of most probable tokens with prob. that add up to top_p or higher are kept for generation
+    temperature: float = .6         # [optional] value used to modulate next token probs
+    top_k: int = 50                 # [optional] number of highest prob. vocabulary tokens to keep for top-k-filtering
+    repetition_penalty: float = 1.0 # parameter for repetition penalty: 1.0 == no penalty
+    length_penalty: int = 1         # [optional] exponential penalty to length used with beam-based generation
+    max_padding_length: int = None  # the max padding length used with tokenizer padding prompts
+
+    tokenizer: Callable = None
+    hf_model: Callable = None
+    safety_checker: List = None
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        #Packages needed
+        from peft import PeftModel
+        from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+         # Set the seeds for reproducibility
+        if self.seed:
+            torch.cuda.manual_seed(self.seed)
+            torch.manual_seed(self.seed)
+
+        # create tokenizer
+        self.tokenizer = None
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False)
+        if self.quantization:
+            bnbconfig = BitsAndBytesConfig(load_in_8bit=True)
+            base_model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False, device_map="auto", quantization_config=bnbconfig)
+        else:
+            base_model = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=self.base_model, local_files_only= False, device_map="auto", torch_dtype=torch.float16)
+
+        if self.peft_model:
+            self.hf_model = PeftModel.from_pretrained(base_model, self.peft_model)
+        else:
+            self.hf_model = base_model
+        self.hf_model.eval()
+
+        # create safety checker
+        self.safety_checker = []
+        if self.enable_salesforce_content_safety:
+            self.safety_checker.append(SalesforceSafetyChecker())
+
+
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+
+    def _call(
+        self,
+        prompt: str = None,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+    ) -> str:
+        
+        context_start = prompt.find("Context:")
+        question_start = prompt.rfind("Question:")
+
+        # check if input is safe:
+        safety_results = [check(prompt[question_start:]) for check in self.safety_checker]
+        are_safe = all([r[1] for r in safety_results])
+        if not are_safe:
+            print("User prompt deemed unsafe.")
+            for method, is_safe, report in safety_results:
+                if not is_safe:
+                    print(method)
+                    print(report)
+            print(f"Skipping the {self.hf_model} inference as the prompt is not safe.")
+            return """It looks as if your question may be unsafe. 
+                    
+                    This may be due to issues relating to toxicity, hate, identity, violence, physical tones, sexual tones, profanity, or biased questions.
+                    
+                    Please try to reformat your question."""
+
+        # prepare input
+        special_tokens = self.tokenizer.special_tokens_map
+        if "[INST]" in special_tokens.get("additional_special_tokens", []):
+            print("INFO - using instructor template")
+            formatted_chat = f"[INST] {prompt} [/INST]"
+            end_tag = "[/INST]"
+
+        elif "<|im_start|>" in special_tokens.get("additional_special_tokens", []) and context_start != -1 and question_start != -1:
+            print("INFO - using chat template")
+            real_context = prompt[context_start+len("Context:"):question_start]
+            question_end = prompt.rfind("Helpful Answer:") if 'Helpful Answer:' in prompt else len(prompt)
+
+            message = [
+                {"role": "system", "content": prompt[:context_start]},
+                {"role": "assistant", "content": f"Here is some useful context:\n {real_context}"},
+                {"role": "user", "content": prompt[question_start+len("Question:"):question_end]},
+            ]
+            formatted_chat = self.tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
+            end_tag = "assistant"
+
+        else:
+            print("INFO - using no template")
+            formatted_chat = prompt
+            end_tag = ""
+
+        batch = self.tokenizer(formatted_chat, return_tensors="pt", add_special_tokens=False)
+        batch = {k: v.to("cuda") for k, v in batch.items()}
+
+        # perform inference
+        with torch.no_grad():
+            outputs = self.hf_model.generate(
+                    **batch,
+                    max_new_tokens=self.max_new_tokens,
+                    do_sample=self.do_sample,
+                    top_p=self.top_p,
+                    temperature=self.temperature,
+                    min_length=self.min_length,
+                    use_cache=self.use_cache,
+                    top_k=self.top_k,
+                    repetition_penalty=self.repetition_penalty,
+                    length_penalty=self.length_penalty,
+                )
+            
+        output_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        # safety check of the model output
+        safety_results = [check(output_text) for check in self.safety_checker]
+        are_safe = all([r[1] for r in safety_results])
+        if not are_safe:
+            print("Model output deemed unsafe.")
+            for method, is_safe, report in safety_results:
+                if not is_safe:
+                    print(method)
+                    print(report)
+            return """The response to your question may be unsafe.
+
+                    This may be due to issues relating to toxicity, hate, identity, violence, physical tones, sexual tones, profanity, or biased questions.
+            
+                    There are two ways to solve this:
+                        - generate the response
+                        - reformat your question so that it does not prompt an unsafe response."""
+
+        return output_text[output_text.rfind(end_tag) + len(end_tag):]
 
 class OpenAILLM(ChatOpenAI):
     """
