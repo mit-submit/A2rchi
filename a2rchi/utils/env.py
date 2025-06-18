c16e10f15776eae9d0a03d438084551b18528b7c
Pietro Martino Lugato <pmlugato@submit76.mit.edu>
2025-06-18 10:15:56 -0400

    small changes

--- a2rchi/utils/env.py
+++ a2rchi/utils/env.py
@@ -4,6 +4,9 @@ def read_secret(secret_name):
     # fetch filepath from env variable
     secret_filepath = os.getenv(f"{secret_name}_FILE")
 
+    if secret_name == "users":
+        return secret_filepath
+
     # read secret from file and return
     with open(secret_filepath, 'r') as f:
         secret = f.read()
