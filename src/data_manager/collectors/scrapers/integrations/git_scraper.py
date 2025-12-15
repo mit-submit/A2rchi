7393a77355668ede43e364ca2f755f18b17ca838
Juan Pablo Salas Galindo <juanp311@submit76.mit.edu>
2025-12-15 14:44:53 -0500

    included updated and creation dates for git docs

--- src/data_manager/collectors/scrapers/integrations/git_scraper.py
+++ src/data_manager/collectors/scrapers/integrations/git_scraper.py
@@ -2,9 +2,10 @@ import re
 import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING, List, Tuple
+from datetime import datetime
 
 from mkdocs.utils.yaml import yaml_load
-from git import Repo
+from git import Repo, Commit
 
 from src.data_manager.collectors.scrapers.scraped_resource import \
     ScrapedResource
@@ -65,9 +66,15 @@ class GitScraper:
     def _prepare_repository(self, url: str) -> Tuple[Path, str]:
         url_dict = self._parse_url(url)
         repo_path = self._clone_repo(url_dict)
-        with (repo_path / "mkdocs.yml").open("r") as file:
-            data = yaml_load(file)
-        base_site_url = data["site_url"] if data["site_url"][-1]=="/" else data["site_url"]+"/"
+        try:
+            with (repo_path / "mkdocs.yml").open("r") as file:
+                data = yaml_load(file)
+            base_site_url = data["site_url"] if data["site_url"][-1]=="/" else data["site_url"]+"/"
+        except Exception:
+            if "rucio" in url_dict["clone_url"]:
+                base_site_url = "https://rucio.cern.ch/documentation/"
+            elif "fts" in url_dict["clone_url"]:
+                base_site_url = "https://fts3-docs.web.cern.ch/fts3-docs/"
         logger.info(f"Site base url: {base_site_url}")
         return repo_path, base_site_url
 
@@ -80,12 +87,15 @@ class GitScraper:
             return []
 
         resources: List[ScrapedResource] = []
+        repo = Repo(repo_path)
         for markdown_path in docs_dir.rglob("*.md"):
             current_url = base_site_url + \
                 markdown_path.relative_to(docs_dir).with_suffix("").as_posix()
 
             logger.info(f"Indexing Git doc: {current_url}")
             text_content = markdown_path.read_text(encoding="utf-8")
+            created_at = self._get_creation_date(repo=repo, file_path=markdown_path)
+            last_updated_at = self._get_last_updated_date(repo=repo, file_path=markdown_path)
             resource = ScrapedResource(
                 url=current_url,
                 content=text_content,
@@ -93,7 +103,9 @@ class GitScraper:
                 source_type="git",
                 metadata={
                     "path": str(markdown_path.relative_to(repo_path)),
-                    "title": str(markdown_path).split('/')[-1].replace('.md','').title()
+                    "title": str(markdown_path).split('/')[-1].replace('.md','').title(),
+                    "created_at": created_at.strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+                    "updated_at": last_updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
                 },
             )
             if len(resource.content)>0:
@@ -106,7 +118,7 @@ class GitScraper:
     def _parse_url(self, url: str) -> dict:
         branch_name = None
 
-        regex_repo_name = r"(?:github|gitlab)\.[\w.]+\/[^\/]+\/([\w.-]+)(?:\.git|\/|$)"
+        regex_repo_name = r"(?:github|gitlab)\.[\w.]+\/[^\/]+\/([\w.-]+)(?:\.git|\/)$"
         match = re.search(regex_repo_name, url, re.IGNORECASE)
         if not match:
             raise ValueError(f"The git url {url} does not match the expected format.")
@@ -116,7 +128,7 @@ class GitScraper:
         if "gitlab" in url:
             clone_from_url = url.replace("gitlab", f"{self.git_username}:{self.git_token}@gitlab")
         elif "github" in url:
-            clone_from_url = url.replace("github", f"{self.git_username}:{self.git_token}@github")
+            clone_from_url = url#.replace("github", f"{self.git_username}:{self.git_token}@github")
         else:
             raise ValueError(f"Unsupported git host in url {url}")
 
@@ -145,11 +157,42 @@ class GitScraper:
         else:
             Repo.clone_from(clone_url, repo_path, branch)
 
-        if not (repo_path / "mkdocs.yml").exists():
-            logger.info(
-                f"Skipping url {original_url}...\nThis repository does not contain mkdocs.yml"
-            )
-            shutil.rmtree(repo_path, ignore_errors=True)
-            raise ValueError("Repository is not MkDocs-based")
-
         return repo_path
+
+    def _get_creation_date(self, repo: Repo, file_path: str) -> datetime:
+        """Retrieves the commit date of the very first commit that introduced the file."""
+        try:
+            commits = list(repo.iter_commits(paths=file_path))
+            
+            if not commits:
+                return None # File not found
+
+            first_commit: Commit = commits[-1]
+            
+            if not first_commit.parents:
+                return first_commit.committed_datetime
+
+            parent_commit = first_commit.parents[0]
+            diffs = first_commit.diff(parent_commit, paths=[file_path])
+            
+            if any(d.change_type == 'A' for d in diffs):
+                return first_commit.committed_datetime
+            
+            return first_commit.committed_datetime
+
+        except Exception as e:
+            print(f"Error retrieving creation date for {file_path}: {e}")
+            return None
+
+    def _get_last_updated_date(self, repo: Repo, file_path: str) -> datetime:
+        """Retrieves the commit date of the last commit that modified the specified file."""
+        try:
+            last_commit: Commit = next(repo.iter_commits(paths=file_path, max_count=1))
+            return last_commit.committed_datetime
+
+        except StopIteration:
+            # File not found in history
+            return None
+        except Exception as e:
+            print(f"Error retrieving last updated date for {file_path}: {e}")
+            return None
